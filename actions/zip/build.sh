ed1fbc8b81bbe343a2ef9c3fd9835e7350606e72
ZephOne <zephone@protonmail.com>
2019-07-10 16:07:29 +0100

    Add  zip action, using a Docker container

---
+++ actions/zip/build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build actions/zip/ -t mmf-action-zip
---
+++ actions/zip/main.js
@@ -0,0 +1,35 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const path = require('path');
+
+const appDir = path.dirname(require.main.filename);
+
+async function shell(command) {
+  console.log('Running: "' + command + '"');
+  const { stdout, stderr } = await exec(command);
+  console.log('stdout:', stdout);
+  console.log('stderr:', stderr);
+}
+
+async function main (payload) {
+    console.log('Zip action received: ', payload);
+    const randomNumber = Math.floor(Math.random() * Math.floor(100000));
+    const compressedFile = '/tmp/' + randomNumber + '.zip';
+
+    const hasBuilt = await shell('bash ' + appDir + '/actions/zip/build.sh');
+
+    const hasZipped = await shell(
+        'docker run --rm -v '
+        + appDir
+        + '/tmp:/tmp mmf-action-zip -rj "$@" '
+        + compressedFile
+        + ' '
+        + payload.map(file => file.location.slice(appDir.length)).join(' ')
+    );
+
+    console.log(appDir + compressedFile);
+
+    return [{location: appDir + compressedFile, name: randomNumber + '.zip'}];
+};
+
+module.exports.run = main
